8f0d6e836188011dd6f78fe87d79953d840ac588
0fill <fikrosek@seznam.cz>
2024-08-25 16:19:05 +0200

    set up
    copleted

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -3,13 +3,20 @@ import sqlite3
 conn = sqlite3.connect("data.sql")
 c = conn.cursor()
 
+c.execute("""CREATE TABLE IF NOT EXISTS directors(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+name TEXT NOT NULL,
+birth_year INTEGER
+)""")
+
+
 c.execute("""CREATE TABLE IF NOT EXISTS movies(
 id INTEGER PRIMARY KEY AUTOINCREMENT,
 title TEXT NOT NULL,
-release_year INTEAGER,
+release_year INTEGER,
 genre TEXT,
-director_id INTEAGER FOREIGN KEY(director_id) REFERENCES directors(id),
-)""")
+director_id INTEGER,
+FOREIGN KEY (director_id) REFERENCES directors(id));""")
 
 c.execute("""CREATE TABLE IF NOT EXISTS actors(
 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -17,13 +24,12 @@ name TEXT NOT NULL,
 birth_year INTEGER
 )""")
 
-c.execute("""CREATE TABLE IF NOT EXISTS directors(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-name TEXT NOT NULL,
-birth_year INTEGER
-)""")
 
 c.execute("""CREATE TABLE IF NOT EXISTS movie_actos(
-movie_id INTEAGER FOREIGN KEY(movie_id) REFERENCES movies(id),
-actor_id INTEGER REFERENCES actors(id) REFERENCES actors(id),
-""")
+movie_id INTEGER, 
+actor_id INTEGER,
+FOREIGN KEY(movie_id) REFERENCES movies(id),
+FOREIGN KEY(actor_id) REFERENCES actors(id))
+;""")
+
+conn.commit()
